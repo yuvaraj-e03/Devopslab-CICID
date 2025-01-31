9303997121010535583132baa4b1947bf7e18492
Yuvaraj <yuvarajeswaran76@gmail.com>
2025-01-31 23:02:07 +0530

    Add files via upload

---
+++ react-project1/src/header.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return(<>
+      <div className='tot-bar'>
+      <img src="logo192.png" width="20px" />
+      <nav><ul className='nav-bar'>
+        <li>Price</li>
+        <li>Price</li>
+        <li>Price</li></ul>
+        </nav>
+        </div>
+        </>
+    )
+  
+  }
