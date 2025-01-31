4dbc8513e4750fc41a27a71b4e0f76a66e47f670
Yuvaraj <yuvarajeswaran76@gmail.com>
2025-01-31 23:41:06 +0530

    Delete react-project1 directory

--- react-project1/src/components/Navbar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Navbar(){
-    return(
-        <nav className="nav-bar">
-            <img src="logo192.png" width="60px"/>
-            <h1>ReactFacts</h1>
-        </nav>
-    )
-}
